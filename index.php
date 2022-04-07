b916d5a73e7f0bc8194d6e533ae5741722102b91
alestucci <alessandrotucci@gmail.com>
2022-04-07 18:04:24 +0200

    Completed (badword not case sensitive)

--- index.php
+++ index.php
@@ -9,11 +9,15 @@
 </head>
 <body>
     <?php
-        $paragrafo = 'Il cammino dell’uomo timorato è minacciato da ogni parte dalle iniquità degli esseri egoisti e dalla tirannia degli uomini malvagi. Benedetto sia colui che nel nome della carità e della buona volontà conduce i deboli attraverso la valle delle tenebre perchè egli è in verita’ il pastore di suo fratello e il ricercatore dei figli smarriti. E la mia giustizia calerà sopra di loro con grandissima vendetta e furiosissimo sdegno, su coloro che proveranno ad ammorbare ed infine a distruggere i miei fratelli. E tu saprai che il mio nome e’ quello del Signore quando farò calare la mia vendetta sopra di te. Ezechiele 25:17'
-        ?>
+        $paragrafo = 'Il cammino dell’uomo timorato è minacciato da ogni parte dalle iniquità degli esseri egoisti e dalla tirannia degli uomini malvagi. Benedetto sia colui che nel nome della carità e della buona volontà conduce i deboli attraverso la valle delle tenebre perchè egli è in verità il pastore di suo fratello e il ricercatore dei figli smarriti. E la mia giustizia calerà sopra di loro con grandissima vendetta e furiosissimo sdegno, su coloro che proveranno ad ammorbare ed infine a distruggere i miei fratelli. E tu saprai che il mio nome è quello del Signore quando farò calare la mia vendetta sopra di te. Ezechiele 25:17';
+        
+    ?>
     
     <div class="container">
         <p><?php echo $paragrafo; ?></p>
+        <h3>Numero di parole: <?=count(explode(" ", $paragrafo))?></h3>
+        <h1>Filtrato con badword</h1>
+        <p><?php echo str_replace(mb_convert_case($_GET["badword"], MB_CASE_LOWER), "***", mb_convert_case($paragrafo, MB_CASE_LOWER)); ?></p>
     </div>
 
 </body>
