d32fc7bf8b45bb7e4dfdf9aa954a630550933d53
alestucci <alessandrotucci@gmail.com>
2022-04-07 16:06:00 +0200

    Root files

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Bad Words</title>
+</head>
+<body>
+    <?php
+        $paragrafo = 'Il cammino dell’uomo timorato è minacciato da ogni parte dalle iniquità degli esseri egoisti e dalla tirannia degli uomini malvagi. Benedetto sia colui che nel nome della carità e della buona volontà conduce i deboli attraverso la valle delle tenebre perchè egli è in verita’ il pastore di suo fratello e il ricercatore dei figli smarriti. E la mia giustizia calerà sopra di loro con grandissima vendetta e furiosissimo sdegno, su coloro che proveranno ad ammorbare ed infine a distruggere i miei fratelli. E tu saprai che il mio nome e’ quello del Signore quando farò calare la mia vendetta sopra di te. Ezechiele 25:17'
+        ?>
+    
+    <div class="container">
+        <p><?php echo $paragrafo; ?></p>
+    </div>
+
+</body>
+</html>
